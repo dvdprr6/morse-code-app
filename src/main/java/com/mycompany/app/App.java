c6d0583065da46fd30aa40300df2cbc41b79a734
David Parr <dvdprr6@gmail.com>
2015-01-28 13:51:39 -0500

    test translating morse code to words

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,11 +3,18 @@ package com.mycompany.app;
 import org.apache.log4j.Logger;
 
 import com.mycompany.app.WordsToMorse;
+import com.mycompany.app.MorseToWords;
 
 public class App {
 	private static final Logger logger = Logger.getLogger(App.class);
     public static void main(String[] args){
-    	WordsToMorse words_to_morse = new WordsToMorse(
-    			System.getProperty("user.dir") + "/words_to_morse.txt");
+//    	WordsToMorse words_to_morse = new WordsToMorse(
+//    			System.getProperty("user.dir") + "/words_to_morse.txt");
+    	
+    	MorseToWords morse_to_words = new MorseToWords(
+    			System.getProperty("user.dir") + "/morse_to_words.txt",
+    			System.getProperty("user.dir") + "/translation.txt");
+    	
+    	morse_to_words.decodeMorse();
     }
 }
